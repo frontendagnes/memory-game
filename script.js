4fc58c73c62f9f85884ab464d692a7478d2ef89b
Agnieszka Kaminska <agnieszka.kaminska@ksiegarnia.edu.pl>
2021-07-31 14:34:08 +0200

    flipCard

--- script.js
+++ script.js
@@ -17,8 +17,8 @@ document.addEventListener("DOMContentLoaded", () => {
   boardItems.sort(() => 0.5 - Math.random());
   console.log(boardItems);
   const items = document.querySelectorAll(".board div");
-  let boardId = []
-  let boardField = []
+  let boardId = [];
+  let boardField = [];
   // tło planszy przed rozpoczęciem gry
   // const setColor = () => {
   //   for (let i = 0; i < items.length; i++) {
@@ -29,40 +29,56 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // sprawdzenie dopasowania pól
   const matchingFields = (index) => {
-    const indexField = items[index].getAttribute("data-id")
-    const backgroundField = boardItems[indexField]
-    console.log(indexField)
-    console.log(backgroundField)
+    const idOne = boardId[0];
+    const idTwo = boardId[1];
+    console.log("i>>", boardId);
+    console.log(boardField);
 
-      boardId.push(indexField)
-      boardField.push(backgroundField)
-      // console.log("id>>", boardId)
-
-    if(boardId.length > 2){
-      boardId.forEach(item => {
-        items[item].style.backgroundColor = "#7979e9cb"
-        boardId = []
-      })
+    if (idOne == idTwo) {
+      console.log("klikałeś w to pole");
+      items[idOne].style.backgroundColor = "#7979e9cb";
+      items[idTwo].style.backgroundColor = "#7979e9cb";
+    }else if (boardField[0] === boardField[1]) {
+      console.log("Trafienie");
+      items[idOne].style.backgroundColor = "white";
+      items[idTwo].style.backgroundColor = "white";
+      items[idOne].removeEventListener("click", flipCard);
+      items[idTwo].removeEventListener("click", flipCard);
+    }else{
+      console.log("Pudło");
+      items[idOne].style.backgroundColor = "#7979e9cb";
+      items[idTwo].style.backgroundColor = "#7979e9cb";
+      items[idOne].removeEventListener("click", flipCard);
+      items[idTwo].removeEventListener("click", flipCard);
     }
 
-    if(boardField.length >= boardItems.length){
-      console.log("Koniec Pól") 
-      boardField = []     
+    if (boardField.length === 2) {
+      boardField = [];
     }
-        console.log("id>>", boardId)
-        console.log("field>>", boardField)
-  }
+    if (boardId.length === 2) {
+      boardId = [];
+    }
+  };
 
   // obrót pola
   const flipCard = (index) => {
-    items[index].style.backgroundColor = boardItems[index]
-    items[index].setAttribute("data-id", index)
+    const backgroundField = boardItems[index];
+    const indexField = items[index].getAttribute("data-id");
     
-    matchingFields(index)
-  }
+    boardField.push(backgroundField);
+    boardId.push(indexField);
+
+    items[index].style.backgroundColor = boardItems[index];
+    items[index].setAttribute("data-id", index);
+
+    if(boardId.length === 2){
+      matchingFields(index);
+    }
+    
+  };
 
   // nasłuch na kliknięcie w pole
-  for(let i = 0; i < items.length; i++){
-    items[i].addEventListener("click", () => flipCard(i))
+  for (let i = 0; i < items.length; i++) {
+    items[i].addEventListener("click", () => flipCard(i));
   }
 });
